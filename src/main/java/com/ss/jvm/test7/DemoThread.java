da295c095aa2f258de0f48d9b92f476c8ee2353f
shisong <shisong@able-elec.com>
2019-03-11 17:38:09 +0800

    提交 jvm 类加载 初始化

---
+++ src/main/java/com/ss/jvm/test7/DemoThread.java
@@ -0,0 +1,68 @@
+package com.ss.jvm.test7;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * DemoThread
+ *
+ * @author shisong
+ * @date 2019/3/11
+ */
+public class DemoThread {
+
+    static class Hello{
+        static {
+            System.out.println(Thread.currentThread().getName() + "init.......");
+
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        void sayHello(){
+            System.out.println("hahhahahaahahahhaahahahaha");
+        }
+
+    }
+
+    /*public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + "start......");
+                Hello hello = new Hello();
+                hello.sayHello();
+                System.out.println(Thread.currentThread().getName() + "end......");
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + "start......");
+                Hello hello = new Hello();
+                hello.sayHello();
+                System.out.println(Thread.currentThread().getName() + "end......");
+            }
+        }).start();
+    }*/
+
+    public static void main(String[] args) {
+        ExecutorService threadPool = Executors.newFixedThreadPool(20);
+        int i = 0;
+        while (i++<20){
+            threadPool.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Thread.currentThread().getName() + "start......");
+                    Hello hello = new Hello();
+                    System.out.println(Thread.currentThread().getName() + "end......");
+                }
+            });
+        }
+    }
+
+}
---
+++ src/main/java/com/ss/jvm/test7/StaticTest.java
@@ -0,0 +1,19 @@
+package com.ss.jvm.test7;
+
+/**
+ * StaticTest
+ *
+ * @author shisong
+ * @date 2019/3/11
+ */
+public class StaticTest {
+
+    static int i = 1;
+
+    static {
+        i =0;
+        System.out.println(i);
+    }
+    // clinit 收集的顺序是顺序收集（从上至下）的。
+    //static int i = 1;clinit 在静态语句块中只能访问定义在静态语句块之前的变量，定义在它之后的变量，只能赋值，不能访问
+}
