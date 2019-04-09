310287ffed35141cb8afdae0da0a8cf744992de4
shisongWjj <540000970@qq.com>
2019-04-09 22:20:47 +0800

    提交  数据结构 循环队列

---
+++ src/main/java/com/ss/dataStructures/queue/LoopQueue.java
@@ -0,0 +1,93 @@
+package com.ss.dataStructures.queue;
+
+public class LoopQueue<E> implements Queue<E> {
+
+    private E[] data;
+    /**
+     * 队列头部的索引
+     */
+    private int front;
+
+    /**
+     * 队列尾部的下一个索引
+     */
+    private int tail;
+
+    /**
+     * 队列中有多少个元素
+     */
+    private int size;
+
+    /**
+     * 有参构造
+     * @param capacity
+     */
+    public LoopQueue(int capacity){
+        //由于当队列满的时候 会有意识的浪费一个空间，所以这里我们创建的时候 应该要比它传入的大1
+        data = (E[])new Object[capacity + 1];
+        size = 0;
+        front = 0;
+        tail = 0;
+    }
+
+    /**
+     * 无参构造
+     */
+    public LoopQueue(){
+        this(10);
+    }
+
+    @Override
+    public void enqueue(E e) {
+        int lastIndex = (tail + 1) % data.length;
+        if(lastIndex ==front){
+            //说明队列已满 需要扩容
+            resize(getCapacity()*2);
+        }
+        data[tail] = e;
+        tail = lastIndex;
+        size++;
+    }
+
+    /**
+     * 扩容
+     * @param newCapacity
+     */
+    private void resize(int newCapacity) {
+        E[] newData = (E[])new Object[newCapacity + 1];
+        for (int i = 0; i < size ; i++){
+            newData[i] = data[(front+i)%data.length];
+        }
+        data = newData;
+        front = 0;
+        tail = size;
+    }
+
+    @Override
+    public E dequeue() {
+        return null;
+    }
+
+    @Override
+    public E getFront() {
+        return null;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return front==tail;
+    }
+
+    /**
+     * 返回队列的容量
+     * @return
+     */
+    public int getCapacity(){
+        return data.length-1;
+    }
+}
