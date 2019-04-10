c244f6664ab581951fcce9e770ca27f9cdb0322c
shisong <shisong@able-elec.com>
2019-04-10 19:47:58 +0800

    提交 简单队列和循环队列的 性能差异

---
+++ src/main/java/com/ss/dataStructures/queue/Main.java
@@ -0,0 +1,46 @@
+package com.ss.dataStructures.queue;
+
+import java.util.Random;
+
+/**
+ * Main
+ *
+ * @author shisong
+ * @date 2019/4/10
+ */
+public class Main {
+
+
+    /**
+     * 测试普通队列和循环队列
+     * @param q
+     * @param opCount
+     * @return
+     */
+    private static double testQueue(Queue<Integer> q,Integer opCount){
+        long startTime = System.nanoTime();
+
+        Random random = new Random();
+        for (int i = 0;i<opCount;i++){
+            q.enqueue(random.nextInt(Integer.MAX_VALUE));
+        }
+        for (int i = 0;i<opCount;i++){
+            q.dequeue();
+        }
+
+        long endTime = System.nanoTime();
+        return (endTime-startTime)/1000000000.0;
+    }
+
+    public static void main(String[] args) {
+        Integer opCount = 100000;
+        LoopQueue<Integer> loopQueue = new LoopQueue<>();
+        double loopTime = testQueue(loopQueue, opCount);
+        System.out.println("loopTime : " + loopTime);
+
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+        double arrayTime = testQueue(arrayQueue, opCount);
+        System.out.println("arrayTime : " + arrayTime);
+    }
+
+}
