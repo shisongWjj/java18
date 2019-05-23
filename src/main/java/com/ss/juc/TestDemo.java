97249511c755363113430e788298c69d8cb352a3
shisongWjj <540000970@qq.com>
2019-05-23 17:09:12 +0800

    提交  用链表实现栈

---
+++ src/main/java/com/ss/juc/TestDemo.java
@@ -0,0 +1,34 @@
+package com.ss.juc;
+
+public class TestDemo {
+
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true){
+                    System.out.println("A");
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true){
+                    System.out.println("B");
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true){
+                    System.out.println("C");
+                }
+            }
+        }).start();
+    }
+
+}
