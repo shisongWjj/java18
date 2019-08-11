9af4d7c2bd6f06e87df7435c8e06d3f0650d7c76
song.shi <song.shi@atzuche.com>
2019-08-11 16:46:04 +0800

    提交 spring 源码解析

--- src/test/java/java18/Collector/CollectorTest.java
+++ src/test/java/java18/Collector/CollectorTest.java
@@ -6,6 +6,7 @@ import java18.utils.ListHelper;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -103,15 +104,17 @@ public class CollectorTest {
 
     @Test
     public void test2(){
-        BigDecimal num1 = new BigDecimal(0.005);
+        BigDecimal num1 = new BigDecimal(0.005).setScale(3, RoundingMode.HALF_UP);
         BigDecimal num2 = new BigDecimal(1000000);
         BigDecimal num3 = new BigDecimal(-1000000);
         //尽量用字符串的形式初始化
-        BigDecimal num12 = new BigDecimal("0.005");
+        BigDecimal num12 = new BigDecimal("0.005").setScale(3, RoundingMode.HALF_UP);
         BigDecimal num22 = new BigDecimal("1000000");
         BigDecimal num32 = new BigDecimal("-1000000");
 
-        //加法
+        System.out.println(num1.subtract(num12).intValue() > 0);
+
+        /*//加法
         BigDecimal result1 = num1.add(num2);
         BigDecimal result12 = num12.add(num22);
         //减法
@@ -140,8 +143,70 @@ public class CollectorTest {
         System.out.println("绝对值用string结果："+result42);
 
         System.out.println("除法用value结果："+result5);
-        System.out.println("除法用string结果："+result52);
+        System.out.println("除法用string结果："+result52);*/
 
     }
 
+    @Test
+    public  void test14(){
+        int a = 150;
+        int b = 50;
+        for(int i = 0 ; i < 10 ;i++){
+            a = a - b;
+            if(a >= 0){
+                System.out.println(b);
+            }else if(a+b>=0){
+                System.out.println(a+b);
+            }else{
+                System.out.println(0);
+            }
+        }
+
+    }
+
+    @Test
+    public void test15(){
+        List<Integer> list2 = getInt(300, 2, 1);
+        System.out.println(list2);
+        List<Integer> list3 = getInt(300, 3, 2);
+        List<Integer> list5 = getInt(300, 5, 4);
+        List<Integer> list7 = getInt(300, 7, 0);
+        list2.retainAll(list3);
+        System.out.println(list2);
+        list2.retainAll(list5);
+        System.out.println(list2);
+        list2.retainAll(list7);
+        System.out.println(list2);
+
+
+    }
+
+    private List<Integer> getInt(Integer max,Integer times,Integer remainder){
+        List<Integer> result = new ArrayList<>();
+        Integer count = 0;
+        while (true){
+            count ++;
+            int temp = times * count + remainder;
+            if(temp > max){
+                return result;
+            }
+            result.add(temp);
+        }
+    }
+
+    public void test16(){
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+       /* Map<String, Map<String, List<Person>>> peopleByStateAndCity
+                = personStream.collect(Collectors.groupingBy(Person::getState,
+                Collectors.groupingBy(Person::getCity)));*/
+    }
 }
