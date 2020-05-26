27222152056533d8d73d837724b5d8a626db0245
shisongWjj <540000970@qq.com>
2020-05-27 00:37:00 +0800

    提交  创建线程

--- src/main/java/com/ss/juc/gupao/InterrupteDemo.java
+++ src/main/java/com/ss/juc/gupao/InterrupteDemo.java
@@ -11,22 +11,88 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class InterrupteDemo {
 
-    public static void main(String[] args) {
-        System.out.println(Thread.currentThread().isInterrupted());
+    /*public static void main(String[] args) {
+        *//*System.out.println(Thread.currentThread().isInterrupted());
         Thread.currentThread().interrupt();
         System.out.println(Thread.currentThread().isInterrupted());
-       /* try {
+       *//**//* try {
             Thread.sleep(1L);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }*/
+        }*//**//*
         LockSupport.park();
         System.out.println(Thread.currentThread().isInterrupted());
         System.out.println(Thread.interrupted());
         System.out.println(Thread.interrupted());
         System.out.println(Thread.currentThread().isInterrupted());
         LockSupport.park();
+        System.out.println(Thread.currentThread().isInterrupted());*//*
+        System.out.println("开始");
         System.out.println(Thread.currentThread().isInterrupted());
+        System.out.println("结束");
+    }*/
+
+    /*public static void main(String[] args) {
+        System.out.println("开始");
+        System.out.println(Thread.currentThread().isInterrupted());
+        System.out.println("结束");
+    }*/
+
+    /*public static void main(String[] args) {
+        System.out.println("开始");
+        System.out.println(Thread.currentThread().isInterrupted());
+        Thread.currentThread().interrupt();
+        System.out.println(Thread.currentThread().isInterrupted());
+        System.out.println("结束");
+    }*/
+
+    /*public static void main(String[] args) {
+        System.out.println("开始");
+        System.out.println(Thread.currentThread().isInterrupted());
+        Thread.currentThread().interrupt();
+        System.out.println(Thread.currentThread().isInterrupted());
+        try {
+            Thread.sleep(1L);
+            System.out.println("阻塞方法结束");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            System.out.println("并没有阻塞，直接抛异常了");
+        }
+        System.out.println(Thread.currentThread().isInterrupted());
+        System.out.println("结束");
+    }*/
+
+    /*public static void main(String[] args) {
+        System.out.println("开始");
+        System.out.println(Thread.currentThread().isInterrupted());
+        Thread.currentThread().interrupt();
+        System.out.println(Thread.currentThread().isInterrupted());
+        LockSupport.park();
+        LockSupport.park();
+        LockSupport.park();
+        System.out.println(Thread.currentThread().isInterrupted());
+        System.out.println("结束");
+    }*/
+
+    /*public static void main(String[] args) {
+        System.out.println("开始");
+        System.out.println(Thread.currentThread().isInterrupted());
+        Thread.currentThread().interrupt();
+        LockSupport.park();
+        Thread.interrupted();
+        System.out.println(Thread.currentThread().isInterrupted());
+        System.out.println("结束");
+    }*/
+
+    public static void main(String[] args) {
+        System.out.println("开始");
+        System.out.println(Thread.currentThread().isInterrupted());
+        Thread.currentThread().interrupt();
+        System.out.println(Thread.currentThread().isInterrupted());
+        System.out.println(Thread.interrupted());
+        System.out.println(Thread.currentThread().isInterrupted());
+        System.out.println(Thread.interrupted());
+        System.out.println("结束");
     }
 
 
