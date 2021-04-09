8fa06059d72bb7c154190b96d53462f88f38f38e
song.shi <song.shi@atzuche.com>
2021-04-09 18:30:35 +0800

    提交 leetcode 16 19 26 27 28

--- src/main/java/com/ss/leetcode/LeetCode0016.java
+++ src/main/java/com/ss/leetcode/LeetCode0016.java
@@ -12,88 +12,70 @@ import java.util.List;
  */
 public class LeetCode0016 {
 
-    /*给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0 且不重
-    复的三元组。
-     注意：答案中不可以包含重复的三元组。
-     示例 1：
-    输入：nums = [-1,0,1,2,-1,-4]
-    输出：[[-1,-1,2],[-1,0,1]]
-     示例 2：
-    输入：nums = []
-    输出：[]
-     示例 3：
-    输入：nums = [0]
-    输出：[]
+    /*给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。返回这三个数的和
+    。假定每组输入只存在唯一答案。
+     示例：
+     输入：nums = [-1,2,1,-4], target = 1
+    输出：2
+    解释：与 target 最接近的和是 2 (-1 + 2 + 1 = 2) 。
      提示：
-     0 <= nums.length <= 3000
-     -105 <= nums[i] <= 105
+     -10^3 <= nums[i] <= 10^3
+     -10^4 <= target <= 10^4
      Related Topics 数组 双指针*/
     public static void main(String[] args) {
-        //int[] nums2 = {-1,0,1,2,-1,-4,-1,0,2,2,2};
-        int[] nums2 = {-1,0,1,2,-1,-4};
-        List<List<Integer>>  area = threeSum(nums2);
+        int[] nums2 = {-1,2,1,-4};
+        int area = threeSumClosest(nums2,1);
         System.out.println(area);
 
     }
 
 
     /**
-     * 解题思路，先将数组 排序
-     * 如果第一个就大于0，那么就直接返回
-     * 如果最后一个就小于0，那么就直接返回
-     * 获取第一个值，作为固定值，如果固定值大于0，就直接结束循环
-     * 然后双指针，一个从N+1开始，一个从尾部开始，
-     * 如果相加 小于0 则头部向后移动，如果大于0，则尾部向前移动，直到尾指针小于等于头指针
      *
-     *
-     * @param nums 数组
-     * @return 结果集
+     * @param nums
+     * @param target
+     * @return
      */
-    public static List<List<Integer>> threeSum(int[] nums) {
-        if(nums == null || nums.length <3){
-            return new ArrayList<>();
+    public static int threeSumClosest(int[] nums, int target) {
+        if(nums == null){
+            return 0;
+        }
+        int result = 0;
+        if(nums.length <= 3){
+            for (int num : nums) {
+                result += num;
+            }
+            return result;
         }
         Arrays.sort(nums);
-        if(nums[0]>0 || nums[nums.length-1] < 0){
-            return new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            result += nums[i];
         }
-        int head,tail;
-        int fixedValue;
-        List<List<Integer>> result = new ArrayList<>();
-        for(int i = 0; i < nums.length - 1 ;i++){
-            fixedValue = nums[i];
-            if(fixedValue > 0){
-                break;
-            }
-            if(i>0 && fixedValue == nums[i-1]){
-                continue;
-            }
-            head = i + 1;
-            tail = nums.length - 1;
-            while (head < tail){
-                int a = nums[head];
-                int b = nums[tail];
-                if(fixedValue + a + b == 0){
-                    result.add(Arrays.asList(fixedValue,a,b));
-                    //针对重复数据做处理
-                    while (head < tail && nums[head] == nums[head+1]){
-                        head+=1;
-                    }
-                    while (head < tail && nums[tail] == nums[tail-1]){
-                        tail-=1;
-                    }
-                    tail--;
-                    head++;
-                }else if(fixedValue + a + b > 0){
-                    tail--;
+        int sum = Math.abs(result - target);
+        for (int i = 0; i < nums.length-2; i++) {
+            int fixValue = nums[i];
+            int left = i+1;
+            int right = nums.length-1;
+            while (left < right){
+                int a = nums[left];
+                int b = nums[right];
+                if(fixValue + a + b < target){
+                    left++;
+                }else if(fixValue + a + b == target){
+                    return target;
                 }else {
-                    head++;
+                    right--;
                 }
+                int temp = Math.abs(fixValue + a + b - target);
+                if(temp < sum){
+                    result = fixValue + a + b;
+                }
+                sum = Math.min(sum, temp);
+
             }
 
         }
         return result;
-
     }
 
 }
